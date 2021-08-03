f0d588bcfdc9839ce7b78282a2f91709f42ddba2
Andre Colon <andre@vidordesigns.com>
2021-08-02 23:00:29 -0700

    added for loop to iterate through list

--- app.js
+++ app.js
@@ -56,6 +56,22 @@ axios.get('https://api.covid19api.com/summary')
         console.log('What is your error?', err)
     });
 
+    let input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("entry");
+    li = ul.querySelector('li');
+
+for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("h3")[0];
+    txtValue = h3.textContent || h3.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+    } else {
+        li[i].style.display = "none";
+    }
+}
+
 // let filterInput = document.getElementById('filterInput');
 // filterInput.addEventListener('keyup', filterNames);
 
